9aef84dafa36d9bca91b2c9b82841d8832f31e4f
ZakiaENZI <ccdu300@gmail.com>
2017-06-09 20:43:18 +0200

    Ajout de pages

--- src/frontEndBundle/Controller/DefaultController.php
+++ src/frontEndBundle/Controller/DefaultController.php
@@ -14,4 +14,52 @@ class DefaultController extends Controller
     {
         return $this->render('frontEndBundle:Pages:homepage.html.twig', ['title' => 'Acceuil']);
     }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction()
+    {
+        return $this->render('frontEndBundle:Pages:about.html.twig', ['title' => 'A Propos']);
+    }
+
+    /**
+     * @Route("/departmentMG", name="departmentmg")
+     */
+    public function departmentmgAction()
+    {
+        return $this->render('frontEndBundle:Pages:departmentmg.html.twig', ['title' => 'Medecine Generale']);
+    }
+
+    /**
+     * @Route("/departmentPsy", name="departmentpsy")
+     */
+    public function departmentpsyAction()
+    {
+        return $this->render('frontEndBundle:Pages:departementpsy.html.twig', ['title' => 'Psychologie']);
+    }
+
+    /**
+     * @Route("/departementProcto", name="departementprocto")
+     */
+    public function departementproctoAction()
+    {
+        return $this->render('frontEndBundle:Pages:departementprocto.html.twig', ['title' => 'Proctologie']);
+    }
+
+    /**
+     * @Route("/departementSophro", name="departementsophro")
+     */
+    public function departementsophroAction()
+    {
+        return $this->render('frontEndBundle:Pages:departementsophro.html.twig', ['title' => 'Sophrologie']);
+    }
+
+    /**
+     * @Route("/departementUrgentiste", name="departementurgentiste")
+     */
+    public function departementurgentisteAction()
+    {
+        return $this->render('frontEndBundle:Pages:departementurgentiste.html.twig', ['title' => 'Urgentiste']);
+    }
 }
